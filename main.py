aec7856280a800772f4ca50a2291b25f067efce4
Thomaspara <thomaspara96@gmail.com>
2024-09-11 15:18:05 +0300

    .

--- main.py
+++ main.py
@@ -75,19 +75,72 @@
 
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.lang import Builder
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
 
-# Load the KV file
-Builder.load_file('views/test_screen.kv')
+class MainScreen(Screen):
+    def __init__(self, **kwargs):
+        super(MainScreen, self).__init__(**kwargs)
+        layout = BoxLayout(orientation='vertical')
 
-class TestScreen(Screen):
-    pass
+        btn_orders = Button(text='Orders', size_hint=(1, 0.2))
+        btn_orders.bind(on_press=self.go_to_orders)
+        layout.add_widget(btn_orders)
+
+        btn_settings = Button(text='Settings', size_hint=(1, 0.2))
+        btn_settings.bind(on_press=self.go_to_settings)
+        layout.add_widget(btn_settings)
+
+        self.add_widget(layout)
+
+    def go_to_orders(self, instance):
+        self.manager.current = 'orders'
+
+    def go_to_settings(self, instance):
+        self.manager.current = 'settings'
+
+class OrdersScreen(Screen):
+    def __init__(self, **kwargs):
+        super(OrdersScreen, self).__init__(**kwargs)
+        layout = BoxLayout(orientation='vertical')
+
+        label = Button(text='Order List Placeholder', size_hint=(1, 0.8))
+        layout.add_widget(label)
+
+        btn_back = Button(text='Back', size_hint=(1, 0.2))
+        btn_back.bind(on_press=self.go_back)
+        layout.add_widget(btn_back)
+
+        self.add_widget(layout)
+
+    def go_back(self, instance):
+        self.manager.current = 'main'
+
+class SettingsScreen(Screen):
+    def __init__(self, **kwargs):
+        super(SettingsScreen, self).__init__(**kwargs)
+        layout = BoxLayout(orientation='vertical')
+
+        label = Button(text='Settings Placeholder', size_hint=(1, 0.8))
+        layout.add_widget(label)
+
+        btn_back = Button(text='Back', size_hint=(1, 0.2))
+        btn_back.bind(on_press=self.go_back)
+        layout.add_widget(btn_back)
+
+        self.add_widget(layout)
+
+    def go_back(self, instance):
+        self.manager.current = 'main'
 
 class MyApp(App):
     def build(self):
         sm = ScreenManager()
-        sm.add_widget(TestScreen(name='test'))
+        sm.add_widget(MainScreen(name='main'))
+        sm.add_widget(OrdersScreen(name='orders'))
+        sm.add_widget(SettingsScreen(name='settings'))
         return sm
 
 if __name__ == '__main__':
     MyApp().run()
+
