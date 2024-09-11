30d8752eec577e82a579e038047b67006e86fefa
Thomaspara <thomaspara96@gmail.com>
2024-09-11 15:30:56 +0300

    .

--- main.py
+++ main.py
@@ -75,95 +75,30 @@
 
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.uix.boxlayout import BoxLayout
+from kivy.lang import Builder
+from kivy.uix.label import Label
 from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
 
-class MainScreen(Screen):
-    def __init__(self, **kwargs):
-        super(MainScreen, self).__init__(**kwargs)
-        layout = BoxLayout(orientation='vertical')
-
-        btn_menu = Button(text='Menu', size_hint=(1, 0.2))
-        btn_menu.bind(on_press=self.go_to_menu)
-        layout.add_widget(btn_menu)
-
-        btn_orders = Button(text='Orders', size_hint=(1, 0.2))
-        btn_orders.bind(on_press=self.go_to_orders)
-        layout.add_widget(btn_orders)
-
-        btn_settings = Button(text='Settings', size_hint=(1, 0.2))
-        btn_settings.bind(on_press=self.go_to_settings)
-        layout.add_widget(btn_settings)
-
-        self.add_widget(layout)
-
-    def go_to_menu(self, instance):
-        self.manager.current = 'menu'
-
-    def go_to_orders(self, instance):
-        self.manager.current = 'orders'
-
-    def go_to_settings(self, instance):
-        self.manager.current = 'settings'
-
-class OrdersScreen(Screen):
-    def __init__(self, **kwargs):
-        super(OrdersScreen, self).__init__(**kwargs)
-        layout = BoxLayout(orientation='vertical')
-
-        label = Button(text='Order List Placeholder', size_hint=(1, 0.8))
-        layout.add_widget(label)
-
-        btn_back = Button(text='Back', size_hint=(1, 0.2))
-        btn_back.bind(on_press=self.go_back)
-        layout.add_widget(btn_back)
-
-        self.add_widget(layout)
-
-    def go_back(self, instance):
-        self.manager.current = 'main'
-
-class SettingsScreen(Screen):
-    def __init__(self, **kwargs):
-        super(SettingsScreen, self).__init__(**kwargs)
-        layout = BoxLayout(orientation='vertical')
-
-        label = Button(text='Settings Placeholder', size_hint=(1, 0.8))
-        layout.add_widget(label)
-
-        btn_back = Button(text='Back', size_hint=(1, 0.2))
-        btn_back.bind(on_press=self.go_back)
-        layout.add_widget(btn_back)
-
-        self.add_widget(layout)
-
-    def go_back(self, instance):
-        self.manager.current = 'main'
+class HomeScreen(Screen):
+    pass
 
 class MenuScreen(Screen):
-    def __init__(self, **kwargs):
-        super(MenuScreen, self).__init__(**kwargs)
-        layout = BoxLayout(orientation='vertical')
+    pass
 
-        label = Button(text='Menu Placeholder', size_hint=(1, 0.8))
-        layout.add_widget(label)
+class OrdersScreen(Screen):
+    pass
 
-        btn_back = Button(text='Back', size_hint=(1, 0.2))
-        btn_back.bind(on_press=self.go_back)
-        layout.add_widget(btn_back)
-
-        self.add_widget(layout)
-
-    def go_back(self, instance):
-        self.manager.current = 'main'
+class TestScreen(Screen):
+    pass
 
 class MyApp(App):
     def build(self):
         sm = ScreenManager()
-        sm.add_widget(MainScreen(name='main'))
+        sm.add_widget(HomeScreen(name='home'))
         sm.add_widget(MenuScreen(name='menu'))
         sm.add_widget(OrdersScreen(name='orders'))
-        sm.add_widget(SettingsScreen(name='settings'))
+        sm.add_widget(TestScreen(name='test'))
         return sm
 
 if __name__ == '__main__':
